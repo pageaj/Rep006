f28b92158ead16e80b255fe30f5943ccdf103b07
pageaj <tony.page@aib7.com>
2016-10-28 23:54:04 +0100

    Initial Commit

---
+++ Rep006/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Rep006
+//
+//  Created by Tony Page on 28/10/2016.
+//  Copyright © 2016 Tony Page. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
